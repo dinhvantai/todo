0474fc825478f7aaaa160fe024f03d5d767ae49f
doanh <pham.van.doanh@framgia.com>
2017-08-28 18:47:15 +0700

    Todo with react

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <div id="content">
+                <span>Hello React!</span>
+            </div>
+        )
+    }
+}
